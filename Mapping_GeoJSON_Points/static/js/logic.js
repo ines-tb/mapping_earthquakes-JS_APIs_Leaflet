bf3764bcb4427eb0b6dad4c31379e60fc2d5c388
ines-tb <ines12tb@gmail.com>
2020-08-28 21:04:27 -0500

    OnEachFeature - Bind Popup to a GeoJSON Point

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -24,22 +24,30 @@ let sanFranAirport =
 				"coordinates":[-122.375,37.61899948120117]}}
 	]};
 
-// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data - pointToLayer Function.
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+// 	  return L.marker(latlng)
+// 	  		.bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+// 	}
+// }).addTo(map);
+
+// Grabbing our GeoJSON data - onEachFeature Function.
 L.geoJSON(sanFranAirport, {
     // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-	  return L.marker(latlng)
-	  		.bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+	  layer.bindPopup("<h2>Airport code: " + layer.feature.properties.faa + "</h2><hr><h3>Airport name: "+ layer.feature.properties.name + "</h3>");
 	}
 }).addTo(map);
-
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	id: "mapbox/dark-v10",
+	id: "mapbox/streets-v11",
 	accessToken: API_KEY
 });
 
